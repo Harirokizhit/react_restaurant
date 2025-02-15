71ece33babfd0ae6cb6c2287e0c790571460545b
Harirokizhit <harisantharam99@gmail.com>
2025-02-15 18:24:30 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -14,7 +14,7 @@ const Navbar = () => {
   return (
     <div className='navbar'>
         <div className='leftSide' id={openLinks ? "open" : "close"}>
-            <img src={Logo} />
+            <img src={Logo} alt="Description of the image"/>
             <div className='hiddenLinks'>
                 <Link to="/">Home</Link>
                 <Link to="/menu">Menu</Link>
